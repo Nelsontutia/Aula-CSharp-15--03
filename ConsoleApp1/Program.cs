acb5d8fe3a790f9bb5f8e2d5fe907a4ccc775b6e
FIC <fic@info.local>
2025-03-15 18:16:59 -0300

    first

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿
+/*int numberA;
+int numberB;
+
+Console.WriteLine("digite o primeiro numero");
+numberA = Convert.ToInt32( Console.ReadLine());
+
+Console.WriteLine("digite o segundo numero");
+numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"a soma de {numberA} e {numberB} = "+(numberA + numberB));
+
+
+
+int numberX;
+int numberY;
+int numberResult;
+
+
+Console.WriteLine("digite o primeiro numero");
+numberX = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("digite o segundo numero");
+numberY = Convert.ToInt32(Console.ReadLine());
+
+numberResult = numberX * numberY;
+
+Console.WriteLine($"a multiplicacao de {numberX} e {numberY} = { numberResult}");
+
+
+using System;
+
+int numberZ;
+int numberW;
+int numberResult;
+
+
+Console.WriteLine("digite o primeiro numero");
+numberZ = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("digite o segundo numero");
+numberW = Convert.ToInt32(Console.ReadLine());
+
+numberResult = numberZ - numberW;
+
+Console.WriteLine($"a subtracao de {numberZ} e {numberW} = {numberResult}");*/
+
+
+
+
+
+
+
+//1.Maior de Dois Números
+
+int numberZ;
+int numberW;
+int numberResult;
+
+
+console.WriteLine("digite o primeiro numero");
+if
+
+
